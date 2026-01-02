a1c36088a6b7c051f30583cef51cf85f3be026af
Lyuboslav Karev <lyubo.karev@gmail.com>
2026-01-02 22:45:50 +0200

    Add live update

--- main.py
+++ main.py
@@ -3,30 +3,37 @@ from nicegui import ui
 from docker_deck.data_gather import get_running_containers
 
 
-dark = ui.dark_mode()
-dark.enable()
+@ui.refreshable
+def main_ui():
+    dark = ui.dark_mode()
+    dark.enable()
+
+    containers = get_running_containers()
+
+    with ui.column().classes("items-center justify-center min-w-screen"):
+        ui.label("Docker Deck").classes("text-h3 mb-6")
+        with ui.grid(columns=3).classes("items-center mb-4"):
+            for container in containers:
+                with ui.card().tight():
+                    ui.label(container.container_name).classes("text-h6")
+                    with ui.card_section():
+                        with ui.grid(columns=2):
+                            ui.label("State:").classes("font-bold")
+                            state_color = (
+                                "text-green-600" if container.state.value.lower() == "running" else "text-red-600"
+                            )
+                            ui.label(container.state.value).classes(state_color)
+                            ui.label("Port:").classes("font-bold")
+                            ui.label(str(container.port) if container.port != -1 else "N/A")
+                            ui.label("Image:").classes("font-bold")
+                            ui.label(container.image)
 
 
-containers = get_running_containers()
+def update_ui():
+    main_ui.refresh()
 
 
-with ui.column().classes("items-center justify-center min-w-screen"):
-    ui.label("Docker Deck").classes("text-h3 mb-6")
-    with ui.grid(columns=3).classes("items-center mb-4"):
-        for container in containers:
-            with ui.card().tight():
-                ui.label(container.container_name).classes("text-h6")
-                with ui.card_section():
-                    with ui.grid(columns=2):
-                        ui.label("State:").classes("font-bold")
-                        state_color = (
-                            "text-green-600" if container.state.value.lower() == "running" else "text-red-600"
-                        )
-                        ui.label(container.state.value).classes(state_color)
-                        ui.label("Port:").classes("font-bold")
-                        ui.label(str(container.port) if container.port != -1 else "N/A")
-                        ui.label("Image:").classes("font-bold")
-                        ui.label(container.image)
-
-
-ui.run(host="0.0.0.0", port=80)
+# ui.run(host="0.0.0.0", port=80)
+main_ui()
+ui.timer(5.0, update_ui)
+ui.run()
